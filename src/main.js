176c023cb7b73c7ff0528b5428fdc0a7be2a31f4
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2018-12-09 10:21:44 -0300

    Adicionado vue2-datatable-component

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Datatable from 'vue2-datatable-component'
 
 import 'va/lib/css'
 
 import 'va/lib/script'
 
 Vue.config.productionTip = false
+
+Vue.use(Datatable)
 
 new Vue({
   router,
