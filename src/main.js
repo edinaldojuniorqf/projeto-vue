39a7d565c811a846d64144e7a9af00f0cfcc417a
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2018-12-11 19:22:18 -0300

    Adicionado vue-the-mask

--- src/main.js
+++ src/main.js
@@ -3,17 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Datatable from 'vue2-datatable-component'
-
+import VueTheMask from 'vue-the-mask'
 import locale from '@/config/locale/'
+
+// imports js
 import './config'
-
-import 'va/lib/css'
-
 import 'va/lib/script'
+
+// imports css
+import 'va/lib/css'
 
 Vue.config.productionTip = false
 
 Vue.use(Datatable, { locale: locale['vue2-datatable-component'] })
+Vue.use(VueTheMask)
 
 new Vue({
   router,
