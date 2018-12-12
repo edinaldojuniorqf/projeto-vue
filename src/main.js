d5aa1465f64fc16b31a0ff657e67aa96427d76d1
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2018-12-12 19:40:42 -0300

    Adicionado vue-select

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import Datatable from 'vue2-datatable-component'
 import VueTheMask from 'vue-the-mask'
+import vSelect from 'vue-select'
 import locale from '@/config/locale/'
 
 // imports js
@@ -11,12 +12,14 @@ import './config'
 import 'va/lib/script'
 
 // imports css
+import './assets/sass/index.scss'
 import 'va/lib/css'
 
 Vue.config.productionTip = false
 
 Vue.use(Datatable, { locale: locale['vue2-datatable-component'] })
 Vue.use(VueTheMask)
+Vue.component('v-select', vSelect)
 
 new Vue({
   router,
