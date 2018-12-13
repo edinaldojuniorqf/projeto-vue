fc2c1219f4bd34cb0c602b2e0d863ac6762fb090
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2018-12-12 22:47:39 -0300

    Componente para actions dos forms

---
+++ src/components/FormAction.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="FormAction">
+    <span>
+      <i class="fa fa-asterisk text-danger"></i> campos obrigatórios
+    </span>
+    <div>
+      <slot name="buttons">
+        <BtnSave @click.native.prevent="handleSaveClick">Salvar</BtnSave>
+        &nbsp;
+        <BtnClear @click.native.prevent="handleClearClick">Limpar</BtnClear>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BtnSave from '@/components/btn/BtnSave'
+import BtnClear from '@/components/btn/BtnClear'
+
+export default {
+  name: 'FormAction',
+
+  components: {
+    BtnSave,
+    BtnClear
+  },
+
+  methods: {
+    handleSaveClick () {
+      this.$emit('saveClick')
+    },
+
+    handleClearClick () {
+      this.$emit('clearClick')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.FormAction {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 40px 0;
+}
+</style>
