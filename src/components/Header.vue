d97f636e66a59282a8e3a7cb68b3be4a85b12326
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2018-12-12 22:29:46 -0300

    Adicionado Header para as páginas

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="Header content-header">
+    <h1>
+      <i :class="icon"></i>
+      {{ title }}
+    </h1>
+    <slot name="actions" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.Header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
