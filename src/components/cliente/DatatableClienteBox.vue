12753a23dafb574fd0bd1b3010add46a4a206a13
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2018-12-14 19:18:54 -0300

    Adicionado campo telefone/celular ao datatable de clientes

--- src/components/cliente/DatatableClienteBox.vue
+++ src/components/cliente/DatatableClienteBox.vue
@@ -9,6 +9,7 @@
 <script>
 import VABox from '@/components//widgets/VABox'
 import DatatableCliente from '@/components/datatable/DatatableCliente.container'
+import tdTelefoneCelular from './comps/td-TelefoneCelular'
 
 export default {
   name: 'DatatableClienteBox',
@@ -28,8 +29,11 @@ export default {
   data () {
     return {
       columns: [
+        { title: 'Cod', field: 'id', sortable: true },
         { title: 'Nome', field: 'nome', sortable: true },
-        { title: 'CPF/CNPJ', field: 'cpfCnpj' }
+        { title: 'CPF / CNPJ', field: 'cpfCnpj' },
+        { title: 'E-mail', field: 'email' },
+        { title: 'Telefone / Celular', field: 'id', tdComp: tdTelefoneCelular }
       ]
     }
   }
---
+++ src/components/cliente/comps/td-TelefoneCelular.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ telefoneCelular }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'td-TelefoneCelular',
+
+  props: ['row'],
+
+  computed: {
+    telefoneCelular () {
+      let value = []
+      let { telefone, celular } = this.row
+
+      if (telefone) {
+        value.push(telefone)
+      }
+
+      if (celular) {
+        value.push(celular)
+      }
+
+      return value.join(' / ')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
