d41c834543526b4734cf7d8e2070068563acf9d9
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2018-12-13 22:24:41 -0300

    Adicionado componente DatatbleDefault

---
+++ src/components/datatable/DatatableDefault.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <datatable
+      :data="data"
+      :total="total"
+      :query="query"
+      :columns="columns" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DatatableDefault',
+
+  props: {
+    data: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+
+    total: {
+      type: Number,
+      default: 0
+    },
+
+    query: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+
+    columns: {
+      type: Array,
+      required: true
+    }
+  },
+
+  watch: {
+    query: {
+      handler (query) {
+        this.$emit('setDatatableQuery', query)
+        this.$emit('loadDatatable')
+      },
+      deep: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
